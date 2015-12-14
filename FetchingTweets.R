7895b292af26843e1d2255080bb204108012e114
rock-nik <nikunjj46@gmail.com>
2015-12-14 13:59:24 +0530

    Tested with some trending topics

--- FetchingTweets.R
+++ FetchingTweets.R
@@ -1,4 +1,4 @@
-searchString <- "#Windows10"
+searchString <- "ArvindKejriwal"
 tweets.list <- searchTwitter( searchString,n=10000 , lang = 'en' )
 tweets.df <- twListToDF(tweets.list)
 write.csv(tweets.df, 'tweets.csv',row.names=F)
--- TwitterAuthentication.R
+++ TwitterAuthentication.R
@@ -1,12 +1,14 @@
 library(devtools)
 library(twitteR)
 library(plyr)
+library(base64enc)
 
 #put your authentication keys here--
 api_key <- "XXXX"
 api_secret <- "XXXX"
 access_token <- "XXXX"
 access_token_secret <- "XXXX"
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
 
